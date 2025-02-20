61640c4a6fd6b30fe6afd85bd9967f2a277ad631
developertanvir2019 <developertanvir2019@gmail.com>
2025-02-20 17:11:35 +0600

    file add complete without add and delete

--- app/file/page.jsx
+++ app/file/page.jsx
@@ -1,9 +1,11 @@
+"use client";
+import { useState } from "react";
 import { fileData } from "../lib/data/file";
 
 const File = () => {
   return (
-    <div>
-      <List file={fileData} />
+    <div className="select-none">
+      <List files={fileData} />
     </div>
   );
 };
@@ -11,11 +13,32 @@ const File = () => {
 export default File;
 
 const List = ({ files }) => {
+  const [isExpand, setIsExpand] = useState({});
   return (
     <div>
       {files?.map((file) => (
-        <div key={file.id}>
-          <span>{file.name}</span>
+        <div key={file.id} className="ps-5">
+          <div className={` flex justify-start items-center gap-1`}>
+            {file?.isFile && (
+              <span
+                onClick={() =>
+                  setIsExpand((prev) => ({
+                    ...prev,
+                    [file.name]: !prev?.[file.name],
+                  }))
+                }
+                className={`text-xl font-semibold cursor-pointer transition-all duration-300 ${
+                  isExpand?.[file?.name] ? "rotate-90" : ""
+                }`}
+              >
+                {">"}
+              </span>
+            )}
+            <span>{file.name}</span>
+          </div>
+          <div className={`${isExpand?.[file.name] ? "block" : "hidden"}`}>
+            {file?.children && <List files={file?.children} />}
+          </div>
         </div>
       ))}
     </div>
