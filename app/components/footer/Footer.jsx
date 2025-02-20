d026d2333f4130b6ee278cb85b74318cabad4334
developertanvir2019 <developertanvir2019@gmail.com>
2025-02-20 14:51:13 +0600

    setup layout

---
+++ app/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>hi footer</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <p>hi nav</p>
+    </div>
+  );
+};
+
+export default Navbar;
