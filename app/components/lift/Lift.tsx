ec50a93755d93df7caff020ddae91a4e5363a12f
developertanvir2019 <developertanvir2019@gmail.com>
2025-03-03 23:49:39 +0600

    complete floor

--- app/components/lift/Lift.tsx
+++ app/components/lift/Lift.tsx
@@ -11,8 +11,11 @@ const Lift = () => {
     { label: "40-50", start: 40 },
   ];
   return (
-    <div className="h-96 bg-slate-200 py-6 grid grid-cols-2 gap-5 ">
+    <div className="h-96 bg-slate-200 py-3 grid grid-cols-2 gap-5 ">
       <div>
+        <p className="text-center text-3xl pb-2 font-bold text-orange-500">
+          Floor Number
+        </p>
         <div className="flex justify-center items-center gap-5">
           {ranges?.map((range) => (
             <div key={range.start}>
@@ -22,20 +25,20 @@ const Lift = () => {
                 }}
                 className={`${
                   range?.start == start ? "bg-green-300" : "bg-slate-600"
-                } w-16 py-2 text-lg text-white font-semibold border rounded-md`}
+                } w-14 py-1 text-lg text-white font-semibold border rounded-md`}
               >
                 {range.label}
               </button>
             </div>
           ))}
         </div>
-        <div className="grid grid-cols-3 gap-5 max-w-48 mx-auto mt-10">
+        <div className="grid grid-cols-3 gap-5 max-w-52 px-5 py-2 rounded-md mx-auto mt-5 bg-cyan-200">
           {[...Array(10)].map((_, i) => (
             <button
               onClick={() => setFloor(i + start)}
               className={`${
-                i + start == floor ? "bg-green-500" : "bg-slate-600"
-              } w-12 h-12 rounded-full  text-lg text-white font-semibold`}
+                i + start == floor ? "bg-red-500" : "bg-cyan-400"
+              } w-9 h-9 rounded-full  text-base text-white font-semibold`}
               key={i}
             >
               {i + start}
@@ -43,7 +46,11 @@ const Lift = () => {
           ))}
         </div>
       </div>
-      <div className="bg-blue-200 h-full">going.....</div>
+      <div className="bg-blue-200 h-full">
+        <h3 className="max-w-48 mx-auto text-7xl font-bold text-center mt-12 bg-gradient-to-r from-green-500 to-purple-400 bg-clip-text text-transparent">
+          {floor}
+        </h3>
+      </div>
     </div>
   );
 };
